f85f6288af09d2e485dc2d222446032e019e65c9
Astha Sharma <asthasharma068@gmail.com>
2024-11-26 14:30:19 -0700

    Updated playwright config for html report

--- playwright.config.js
+++ playwright.config.js
@@ -1,8 +1,11 @@
 // playwright.config.js
-module.exports = {
-    timeout: 30000,
-    retries: 1,
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+    timeout: 30000, // Set timeout for each test
+    retries: 1, // Retry failing tests once
     use: {
-        headless: true
-    }
-};
+        headless: true, // Run tests in headless mode
+    },
+    reporter: [['html', { outputFolder: 'playwright-report' }]], // Use the HTML reporter
+});
